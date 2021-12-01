18bb6a56ff9b9b547157c75eeb47aebc4de5f518
Anders Kalhauge <aka@sa.dk>
2021-12-01 17:18:10 +0100

    selections cont.

--- WebConsoleConnector/Form/Selection.cs
+++ WebConsoleConnector/Form/Selection.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text;
 using WebConsoleConnector.Form.Actions;
 using WebConsoleConnector.Utilities;
@@ -18,15 +19,36 @@ namespace WebConsoleConnector.Form
     {
         public bool Selected { get; set; }
         public string Value { get; }
+        public Selection Selection { get; }
+
+        public Option(Selection selection, string value, bool selected)
+        {
+            Selection = selection;
+            Value = value;
+            Selected = selected;
+        }
+
+        public Option(Selection selection, string value) : this(selection, value, false) { }
 
         public override void Accept(StringBuilder builder, string indent)
         {
-            throw new NotImplementedException();
+            switch ((Parent as Selection).Type)
+            {
+                case SelectionType.Dropdown:
+                    builder.AppendIndentedLine(indent, $"<option id='{Id}'>{Value}</option>");
+                    break;
+            }
         }
 
         public override bool Handle(IAction action)
         {
-            throw new NotImplementedException();
+            switch (action)
+            {
+                case ClickAction clickAction:
+                    OnClick?.Invoke(this);
+                    return true;
+            }
+            return false;
         }
     }
 
@@ -34,6 +56,7 @@ namespace WebConsoleConnector.Form
     {
         public SelectionType Type { get; }
         public bool MultiSelect { get; }
+        public List<Option> Options { get; } = new();
 
         public Selection(SelectionType type, bool multiSelect)
         {
@@ -46,8 +69,9 @@ namespace WebConsoleConnector.Form
             switch (Type)
             {
                 case SelectionType.Dropdown:
-                    builder.AppendIndentedLine(indent, $"<selection id='{Id}'>");
-                    builder.AppendIndentedLine(indent, $"</selection>");
+                    builder.AppendIndentedLine(indent, $"<select id='{Id}'>");
+                    foreach (Option option in Options) Accept(builder, indent == null ? null : $"  {indent}"); 
+                    builder.AppendIndentedLine(indent, $"</select>");
                     break;
             }
         }
